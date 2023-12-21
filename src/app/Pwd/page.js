bae4d93d87281fbbbe1ec8e3f91c6af0583e1e5d
jpvaldez <jpvaldez@dswd.gov.ph>
2023-12-21 10:53:58 +0800

    update

--- src/app/Pwd/page.js
+++ src/app/Pwd/page.js
@@ -18,6 +18,7 @@ import CashReliefAssistance from "../components/CashReliefAssistance";
 import ReferralAssistanceCard from "../components/ReferralAssistanceCard";
 import PwdCard from "../components/PwdCard";
 
+
 export default function Faqs() {
   const [openDiv, setOpenDiv] = useState(true);
   const [itemNo, setItemNo] = useState(0);
@@ -73,7 +74,7 @@ export default function Faqs() {
           />
         </div>
         <div className="flex items-center">
-         {/*  <input
+          {/*  <input
             type="text"
             className="px-4 py-2 border-b border-gray-300 focus:outline-none bg-transparent"
           />
@@ -92,7 +93,7 @@ export default function Faqs() {
           <Image
             width={80}
             height={80}
-            src={"/images/pwd.png"} 
+            src={"/images/pwd.png"}
             alt="pic"
             style={{ width: "auto", height: "auto" }}
           />
@@ -110,7 +111,7 @@ export default function Faqs() {
           </h2> */}
         </span>
       </div>
-      <div className="flex flex-row w-full overflow-y-auto custom-scrollbar-hidden">
+      {/* <div className="flex flex-row w-full overflow-y-auto ">
         <div
           className={`${
             openDiv ? "w-full " : "lg:w-1/3 "
@@ -131,13 +132,63 @@ export default function Faqs() {
             />
             <div className={`${openDiv ? "hidden" : "block"} `}>
               {contentData.map((item) =>
-               /*  item.id === itemNo ? <p key={item.id}>{item.content}</p> : null */
+         
                item.id === itemNo ?  <div key={item.id} dangerouslySetInnerHTML={{ __html: item.content }} /> : null
               )}
             </div>
           </div>
         </div>
+      </div> */}
+
+      <div className="w-full mt-24" id="accordion-open" data-accordion="open">
+        <h2 id="accordion-open-heading-1">
+          <button type="button" className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-open-body-1" aria-expanded="true" aria-controls="accordion-open-body-1">
+            <span className="flex items-center"><svg className="w-5 h-5 me-2 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path></svg> Description</span>
+            <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5" />
+              
+            </svg>
+          </button>
+        </h2>
+        <div id="accordion-open-body-1" className="hidden" aria-labelledby="accordion-open-heading-1">
+          <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+            <p className="mb-2 text-gray-500 dark:text-gray-400">As a part of Auxiliary Social Services and in support to the Magna Carta for Persons with Disabilities the DSWD through our Field Offices provides augmentation support under the Persons with Disability budget in assistive devices, medical assistance, Educational assistance, Burial Assistance and Livelihood Assistance for Persons with Disabilities specifically with those Physical Disabilities, Visual Disability and Learners with Disabilities. In doing this, the Department’s aim is to contribute to the Physical Restoration, self, and social enhancement of Persons with Disabilities to attain more meaningful and contributing members of society.</p>
+          {/*   <p className="text-gray-500 dark:text-gray-400">Check out this guide to learn how to <a href="/docs/getting-started/introduction/" className="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p>
+        */}   </div>
+        </div>
+        <h2 id="accordion-open-heading-2">
+          <button type="button" className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-open-body-2" aria-expanded="false" aria-controls="accordion-open-body-2">
+            <span className="flex items-center"><svg className="w-5 h-5 me-2 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path></svg>What are the requirements?</span>
+            <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5" />
+            </svg>
+          </button>
+        </h2>
+        <div id="accordion-open-body-2" className="hidden" aria-labelledby="accordion-open-heading-2">
+          <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
+            <p className="mb-2 text-gray-500 dark:text-gray-400">As a part of Auxiliary Social Services and in support to the Magna Carta for Persons with Disabilities the DSWD through our Field Offices provides augmentation support under the Persons with Disability budget in assistive devices, medical assistance, Educational assistance, Burial Assistance and Livelihood Assistance for Persons with Disabilities specifically with those Physical Disabilities, Visual Disability and Learners with Disabilities. In doing this, the Department’s aim is to contribute to the Physical Restoration, self, and social enhancement of Persons with Disabilities to attain more meaningful and contributing members of society.</p>
+            {/* <p className="text-gray-500 dark:text-gray-400">Check out the <a href="https://flowbite.com/figma/" className="text-blue-600 dark:text-blue-500 hover:underline">Figma design system</a> based on the utility classes from Tailwind CSS and components from Flowbite.</p>
+           */}</div>
+        </div>
+        <h2 id="accordion-open-heading-3">
+          <button type="button" className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-open-body-3" aria-expanded="false" aria-controls="accordion-open-body-3">
+            <span className="flex items-center"><svg className="w-5 h-5 me-2 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path></svg> Who can avail?</span>
+            <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5" />
+            </svg>
+          </button>
+        </h2>
+        <div id="accordion-open-body-3" className="hidden" aria-labelledby="accordion-open-heading-3">
+          <div className="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
+            <p className="mb-2 text-gray-500 dark:text-gray-400">Filipino Children and Persons with Disabilities</p>
+            {/*  <ul className="ps-5 text-gray-500 list-disc dark:text-gray-400">
+              <li><a href="https://flowbite.com/pro/" className="text-blue-600 dark:text-blue-500 hover:underline">Flowbite Pro</a></li>
+              <li><a href="https://tailwindui.com/" rel="nofollow" className="text-blue-600 dark:text-blue-500 hover:underline">Tailwind UI</a></li>
+            </ul> */}
+          </div>
+        </div>
       </div>
+
 
       <div className="items-center text-red-900 font-semibold flex flex-row   gap-3  w-full mt-12s md:mt-auto">
         <IoIosArrowRoundBack />
