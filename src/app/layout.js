85895dcd8403b032e6e93059327336820124d4b7
jpvaldez <jpvaldez@dswd.gov.ph>
2023-12-21 11:32:13 +0800

    iupdate

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
       </head>
       <body>
         <div id="root">{children}</div>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
+        <script async src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
       </body>
     </html>
   );
